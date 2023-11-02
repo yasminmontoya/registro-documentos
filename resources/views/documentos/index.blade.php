9c21eadfab70b2c566279ca53cd4dba48a32ea94
yasminmontoya <yaz.lorena@gmail.com>
2023-11-02 16:30:20 -0500

    crear controlador, rutas y vista para documentos

---
+++ resources/views/documentos/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Documentos') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Codigo</th>
+                            <th scope="col">Tipo</th>
+                            <th scope="col">Proceso</th>
+                            <th scope="col">Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($documentos as $documento)
+                            <tr>
+                                <th scope="row">{{ $documento->id }}</th>
+                                <td>{{ $documento->nombre }}</td>
+                                <td>{{ $documento->codigo }}</td>
+                                <td>{{ $documento->idTipo }}</td>
+                                <td>{{ $documento->idProceso }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
