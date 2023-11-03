e0a27ad581244c0138f761bf3ab7b90ae2b482e8
yasminmontoya <yaz.lorena@gmail.com>
2023-11-03 11:46:39 -0500

    modificar seeder de documento

--- database/seeders/DocumentoTableSeeder.php
+++ database/seeders/DocumentoTableSeeder.php
@@ -20,7 +20,7 @@ class DocumentoTableSeeder extends Seeder
         $documento = new Documento();
         $documento->nombre = "INSTRUCTIVO DE DESARROLLO";
         $documento->codigo = "INS-ING-1";
-        $documento->contenido = "CONTENIDO DEL DOCUMENTO";
+        $documento->contenido = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum massa arcu, blandit et eleifend eget, efficitur vitae orci. Maecenas viverra tempor purus vel congue. Ut dignissim tempus nisl eu tristique. Cras quis arcu iaculis arcu volutpat dictum sit amet volutpat mauris. Praesent consequat ornare lacus, eget porttitor sem iaculis in. Proin diam dui, dignissim ut eros ut, scelerisque volutpat nisi. Sed rhoncus commodo augue, eu faucibus ipsum ultrices sit amet. Mauris sed blandit elit. Morbi euismod ligula consequat diam aliquam, et condimentum eros blandit.";
         $documento->tipo_id = "1";
         $documento->proceso_id = "1";
         $documento->save();
