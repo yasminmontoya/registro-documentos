4a97c4595a917cefad384b16a7b200dd34ae3a49
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 01:00:23 -0500

    Definir mi propia clase Request (correccion)

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+use App\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -49,7 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = App\Custom\Http\Request::capture()
+    $request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
